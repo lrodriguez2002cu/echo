859aa14c70b61876580d55ff2c0b8c516db4735a
Luis E. Rodriguez <luis.rodriguez@es.gt.com>
2022-03-29 15:37:47 +0200

    Add project files.

---
+++ Controllers/EchoController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace echo.Controllers
+{
+    [ApiController]
+    [Route("/{*path}")]
+
+    public class EchoController : ControllerBase
+    {
+
+        private readonly ILogger<EchoController> _logger;
+
+        public EchoController(ILogger<EchoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        //[HttpPost]
+        //[HttpPut]
+        //[HttpDelete]
+        //[HttpPut]
+        //[HttpPatch]
+        public IActionResult Get()
+        {
+
+
+            return new JsonResult(new EchoInfo()
+            {
+                Date = DateTime.Now,
+                RequestData = new Dictionary<string, object> {
+                   { "Headers",  Request.Headers},
+                   { "Path", Request.Path},
+                   //{ "Body", Request.Body},
+               }
+            });
+        }
+    }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,10 @@
+namespace echo
+{
+    public class EchoInfo
+    {
+        public DateTime Date { get; set; }
+
+        public Dictionary<string, object> RequestData { get; set; }
+
+    }
+}
