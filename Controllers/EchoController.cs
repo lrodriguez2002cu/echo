fa322cf7281998cca3c8281d517fb9b8635913d4
Luis E. Rodriguez <luis.rodriguez@es.gt.com>
2022-04-05 11:04:29 +0200

    Adding the query to the info provided

--- Controllers/EchoController.cs
+++ Controllers/EchoController.cs
@@ -36,15 +36,21 @@ namespace echo.Controllers
         {
             var resonseStatus = GetResponseStatusCode();
 
-            _logger.LogInformation("Request: '{req}',  ResponseStatus: {responseStatus} , Path: '{path}', Host: {host}, MachineName: {machineName}, headers: {headers}",
+            _logger.LogInformation("Request: '{req}',  ResponseStatus: {responseStatus} , Path: '{path}', " +
+                "Host: {host}, MachineName: {machineName}, " +
+                "headers: {headers}, " +
+                "Query: {query}",
                 this.HttpContext.TraceIdentifier, resonseStatus, Request.Path, Request.Host, Environment.MachineName,
-                Request.Headers.Select(a => $"'{a.Key}: {a.Value}'").Aggregate((e, a) => e + ", " + a));
+                Request.Headers.Select(a => $"'{a.Key}: {a.Value}'").Aggregate((e, a) => e + ", " + a),
+                Request.Query.Select(q => $"'{q.Key}: {q.Value}'").Aggregate((e, a) => e + ", " + a)
+                );
 
             return new JsonResult(new EchoInfo()
             {
                 Date = DateTime.Now,
                 RequestData = new Dictionary<string, object> {
-                   { "Headers",  Request.Headers},
+                   { "Headers",  Request.Headers.ToDictionary(h=> h.Key, h=> h.Value)},
+                   { "Query",  Request.Query.ToDictionary(e => e.Key, v=> v.Value)},
                    { "Path", Request.Path},
                    { "Host", Request.Host },
                    { "EnvHost", Environment.MachineName},
